b996b8ea92ec19bd768c392ea702225039e3c22d
Mateus Siqueira Salomão <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-09-21 16:59:00 -0300

    Primeira Atividade ASP NET

---
+++ Atividade1 ASP Net/Controllers/AtividadeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Atividade1_ASP_Net.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class AtividadeController
+    {
+        [HttpGet]
+        public string executaAtividade()
+        {          
+            return "Hello Word!";
+        }
+        [HttpGet("/bsm")]
+        public string GenerationBSM()
+        { 
+            return "\n\nRESPONSABILIDADE PESSOAL\n\nMENTALIDADE DE CRESCIMENTO\n\nORIENTAÇÃO PARA O FUTURO\n\nPERSISTÊNCIA\n\nCOMUNICAÇÃO\n\nTRABALHO EM EQUIPE\n\nPROATIVIDADE\n\nORIENTAÇÃO AO DETALHE"; 
+        }
+        [HttpGet("/objetivo")]
+        public string Objetivo()
+        {
+            return "Feedback e aprendizagem contínua";
+        }
+    }
+}
